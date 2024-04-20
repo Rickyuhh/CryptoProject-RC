fd16b1028d699afba157d05e595ffdb6d2d13829
Rickyuhh <146773128+Rickyuhh@users.noreply.github.com>
2024-04-20 17:44:07 -0400

    Few Things
    Made few changes such as format layout, added a menu to load host and guest image.

--- src/cryptoproject/rc/ImageSteg.java
+++ src/cryptoproject/rc/ImageSteg.java
@@ -4,12 +4,15 @@
  */
 package cryptoproject.rc;
 
+import java.awt.HeadlessException;
 import java.awt.Image;
 import java.io.File;
+import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -21,20 +24,23 @@ public class ImageSteg extends javax.swing.JFrame {
 
     /**
      * Creates new form ImageSteg
+     * @param lbl
+     * @param f
      */
     public void placeImage(JLabel lbl, File f) {
-        try
-        {
+            try {
             Image img = ImageIO.read(f);
-            Image imgScaled = img.getScaledInstance(lbl.getWidth(), lbl.getHeight(), Image.SCALE_SMOOTH);
-            lblImage.setIcon(new ImageIcon(imgScaled));
+            if (img != null) {
+                Image imgScaled = img.getScaledInstance(lbl.getWidth(), lbl.getHeight(), Image.SCALE_SMOOTH);
+                lbl.setIcon(new ImageIcon(imgScaled));  // Set icon on the passed label, not specifically lblImageGuest
+            } else {
+                JOptionPane.showMessageDialog(this, "File is not a valid image", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (HeadlessException | IOException e) {
+            JOptionPane.showMessageDialog(this, "Error loading image: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
-        catch(Exception e)
-        {
-            System.out.println(e);
-        }
-        
     }
+    
     public ImageSteg() {
         initComponents();
     }
@@ -48,74 +54,86 @@ public class ImageSteg extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnLoadImage = new javax.swing.JButton();
-        lblImage = new javax.swing.JLabel();
+        lblImageGuest = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
-        lblImageEnc = new javax.swing.JLabel();
+        lblImageHost = new javax.swing.JLabel();
         lblEnc = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuGstImg = new javax.swing.JMenuItem();
+        jMenuHstImg = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        btnLoadImage.setText("Load Image");
-        btnLoadImage.addActionListener(new java.awt.event.ActionListener() {
+        lblImageGuest.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        jLabel1.setText("Guest Image");
+
+        lblImageHost.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        lblEnc.setText("Host Image");
+
+        jMenu1.setText("File");
+
+        jMenuGstImg.setText("Select Guest Image");
+        jMenuGstImg.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnLoadImageActionPerformed(evt);
+                jMenuGstImgActionPerformed(evt);
             }
         });
+        jMenu1.add(jMenuGstImg);
 
-        lblImage.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jMenuHstImg.setText("Select Host Image");
+        jMenuHstImg.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuHstImgActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuHstImg);
 
-        jLabel1.setText("Original Image");
+        jMenuBar1.add(jMenu1);
 
-        lblImageEnc.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
 
-        lblEnc.setText("Encrypted Image");
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(337, 337, 337)
-                        .addComponent(jLabel1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(166, 166, 166)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lblImageEnc, javax.swing.GroupLayout.PREFERRED_SIZE, 454, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(lblImage, javax.swing.GroupLayout.PREFERRED_SIZE, 454, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addGap(475, 475, 475)
-                .addComponent(btnLoadImage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(35, 35, 35))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(330, 330, 330)
+                .addGap(337, 337, 337)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(lblEnc)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(370, 370, 370))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(166, 166, 166)
+                .addComponent(lblImageGuest, javax.swing.GroupLayout.PREFERRED_SIZE, 454, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 15, Short.MAX_VALUE)
+                .addComponent(lblImageHost, javax.swing.GroupLayout.PREFERRED_SIZE, 454, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(139, 139, 139))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(30, 30, 30)
-                        .addComponent(btnLoadImage))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(13, 13, 13)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(lblImage, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
-                .addComponent(lblEnc)
-                .addGap(18, 18, 18)
-                .addComponent(lblImageEnc, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(42, 42, 42))
+                .addGap(13, 13, 13)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblEnc))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(lblImageGuest, javax.swing.GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE)
+                    .addComponent(lblImageHost, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(359, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnLoadImageActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoadImageActionPerformed
+    private void jMenuGstImgActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuGstImgActionPerformed
         File f = null;
         JFileChooser fc = new JFileChooser();
         FileFilter ff = new FileNameExtensionFilter("Image File", "jpeg", "jpg", "png", "tif");
@@ -125,11 +143,27 @@ public class ImageSteg extends javax.swing.JFrame {
         if (result == JFileChooser.APPROVE_OPTION) {
             f = fc.getSelectedFile();
         }
-        placeImage(lblImage, f);
+        placeImage(lblImageGuest, f);
         //for text
         //btnOpenTextFile
         //File f = new File (filel.txt);
-    }//GEN-LAST:event_btnLoadImageActionPerformed
+    }//GEN-LAST:event_jMenuGstImgActionPerformed
+
+    private void jMenuHstImgActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuHstImgActionPerformed
+        File f1 = null;
+        JFileChooser fc = new JFileChooser();
+        FileFilter ff = new FileNameExtensionFilter("Image File", "jpeg", "jpg", "png", "tif");
+        fc.setFileFilter(ff);
+        //        fc.addChoosableFileFilter(new FileNameExtensionFilter("Image File", "jpeg", "jpg", "png", "tif"));
+        int result = fc.showOpenDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            f1 = fc.getSelectedFile();
+        }
+        placeImage(lblImageHost, f1);
+        //for text
+        //btnOpenTextFile
+        //File f = new File (filel.txt);
+    }//GEN-LAST:event_jMenuHstImgActionPerformed
 
     /**
      * @param args the command line arguments
@@ -159,18 +193,20 @@ public class ImageSteg extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ImageSteg().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new ImageSteg().setVisible(true);
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnLoadImage;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuGstImg;
+    private javax.swing.JMenuItem jMenuHstImg;
     private javax.swing.JLabel lblEnc;
-    private javax.swing.JLabel lblImage;
-    private javax.swing.JLabel lblImageEnc;
+    private javax.swing.JLabel lblImageGuest;
+    private javax.swing.JLabel lblImageHost;
     // End of variables declaration//GEN-END:variables
 }
